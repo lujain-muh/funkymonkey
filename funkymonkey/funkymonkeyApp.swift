7f829e85c1e0d8a5bda918c4c76e0abe6e5c957a
Lujain <lujain_mohammed@outlook.sa>
2023-02-07 01:36:26 +0300

    Initial Commit

---
+++ funkymonkey/funkymonkeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  funkymonkeyApp.swift
+//  funkymonkey
+//
+//  Created by lujain mohammed on 16/07/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct funkymonkeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
